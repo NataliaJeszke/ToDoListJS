70cfc7510891fad1314a1d006a33e983d13a1a16
NataliaJeszke <nataliajeszke8@gmail.com>
2022-12-29 19:15:57 +0100

    create list of tasks with class, new <p>

--- script.js
+++ script.js
@@ -5,7 +5,28 @@ let listArray=[];
 function addFunction(){
     task = document.getElementById("inputTask").value;
     localStorage.setItem(`Task`, `${task}`);
+
+    getTask=localStorage.getItem(`Task`);
+
+    let myTaskList = document.getElementById("taskList");
+             
+    let checkbox = document.createElement('input');
+     
+    checkbox.type = "checkbox";
+    checkbox.name = "name";
+    checkbox.value = "value";
+    checkbox.id = "id";
+     
+    let label = document.createElement('label');
+
+    label.htmlFor = "id";
+    label.className="label"
+    label.innerHTML=`${getTask} <br/>`;
+     
+    myTaskList.appendChild(checkbox);
+    myTaskList.appendChild(label);
 };
+
 
 document.getElementById("submit").addEventListener("click", pushToArray);
 
@@ -15,14 +36,7 @@ function pushToArray(){
     
     localStorage.setItem("array", JSON.stringify(listArray));
     let storedArray=JSON.parse(localStorage.getItem("array"));
-    console.log(storedArray);
-
-    const paragraph = document.createElement("p");
-    paragraph.setAttribute("class", "listParagraph");
-    const node = document.createTextNode(`${task}`);
-    paragraph.appendChild(node);
-    const element = document.getElementById("taskList")
-    element.appendChild(paragraph);
+    // console.log(storedArray);
 
 }
 

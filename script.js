d9b695040341068b88861a44551d4ec132dbfcc3
NataliaJeszke <nataliajeszke8@gmail.com>
2022-12-29 20:45:30 +0100

    show elements from array to HTML

--- script.js
+++ script.js
@@ -1,46 +1,39 @@
-let task = "";
 let listArray=[];
-
-
-function addFunction(){
-    task = document.getElementById("inputTask").value;
-    localStorage.setItem(`Task`, `${task}`);
-
-    getTask=localStorage.getItem(`Task`);
-
-    let myTaskList = document.getElementById("taskList");
-             
-    let checkbox = document.createElement('input');
-     
-    checkbox.type = "checkbox";
-    checkbox.name = "name";
-    checkbox.value = "value";
-    checkbox.id = "id";
-     
-    let label = document.createElement('label');
-
-    label.htmlFor = "id";
-    label.className="label"
-    label.innerHTML=`${getTask} <br/>`;
-     
-    myTaskList.appendChild(checkbox);
-    myTaskList.appendChild(label);
-};
-
 
 document.getElementById("submit").addEventListener("click", pushToArray);
 
 function pushToArray(){
-    let getFromLocal=localStorage.getItem("Task")
-    listArray.push(`${getFromLocal}`);
+    // let getFromLocal=localStorage.getItem("Task")
+    task = document.getElementById("inputTask").value;
+    listArray.push(`${task}`);
     
     localStorage.setItem("array", JSON.stringify(listArray));
     let storedArray=JSON.parse(localStorage.getItem("array"));
-    // console.log(storedArray);
+
+    let taskFromArray = storedArray[storedArray.length-1];
+
+    let myTaskList = document.getElementById("taskList");
+         
+    let checkbox = document.createElement('input');
+ 
+    checkbox.type = "checkbox";
+    checkbox.name = "name";
+    checkbox.value = "value";
+    checkbox.className = "id";
+    
+    let label = document.createElement('label');
+
+    label.htmlFor = "id";
+    label.className="label"
+    label.innerHTML=`${taskFromArray} <br/>`;
+    
+    myTaskList.appendChild(checkbox);
+    myTaskList.appendChild(label);
+    
+    console.log(storedArray.length);
 
 }
-
-//4. Taski mają się pojawiać jeden pod drugim;
+//5. 
 
 
 

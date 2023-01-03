6b3d888bd24ae6777edd36d5c2eaa5df335fd7a9
NataliaJeszke <nataliajeszke8@gmail.com>
2023-01-03 18:19:09 +0100

    shows tasks from local storage after refresh

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let storedArray=[];
 
 document.getElementById("submit").addEventListener("click", pushToArray);
 
+
+//Function that pushes elements into the array and sends array to local storage. Also displays tasks in a div with id "taskList".
 function pushToArray(){
     // let getFromLocal=localStorage.getItem("Task")
     task = document.getElementById("inputTask").value;
@@ -36,10 +38,9 @@ function pushToArray(){
     
     console.log(storedArray.length);
 
-
 }
 
-//5. Zapamiętanie strony po odswieżeniu przeglądarki. Zmiana zachowania reset button.
+//Function RESET for button "Reset".-> Removes tasks from local storage, reloads page.
 
 function resetFunction(){
     storedArray=JSON.parse(localStorage.getItem("array"));
@@ -48,6 +49,41 @@ function resetFunction(){
     document.location.reload();
 }
 
+//Function that shows tasks saved in local storage.
+
+window.onload = function(){
+    storedArray=JSON.parse(localStorage.getItem("array"));
+
+    function loopArray(){
+        let taskFromArray="";
+        for (i=0; i<storedArray.length; i++){
+            taskFromArray= storedArray[i];
+    
+    let myTaskList = document.getElementById("taskList");
+         
+    let checkbox = document.createElement('input');
+ 
+    checkbox.type = "checkbox";
+    checkbox.name = "name";
+    checkbox.value = "value";
+    checkbox.className = "id";
+    checkbox.checked=false;
+
+
+    let label = document.createElement('label');
+
+    label.htmlFor = "id";
+    label.className="label";
+    label.innerHTML=`${taskFromArray} <br/>`;
+    
+    myTaskList.appendChild(checkbox);
+    myTaskList.appendChild(label);
+        }
+
+    }
+
+    loopArray();
+}
 
 
 

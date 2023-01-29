255df6d91b162607436601591e78d1eeefacade7
NataliaJeszke <nataliajeszke8@gmail.com>
2023-01-29 20:39:35 +0100

    changes after code review

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById("resetBtn").addEventListener("click", resetFunction);
 
 // Variables for DOM elements
 const $myTaskList = document.getElementById("taskList");
-const $elements = document.getElementsByClassName("id");
+const $elements = document.getElementsByClassName("element");
 const $taskContent = document.getElementsByClassName("label");
 // Variable for array of objects from local storage
 const $storedArray = JSON.parse(localStorage.getItem("array"));
@@ -25,7 +25,7 @@ function addTaskToList() {
     checkbox.type = "checkbox";
     checkbox.name = "name";
     checkbox.value = "value";
-    checkbox.className = "id";
+    checkbox.className = "element";
     checkbox.checked = false;
 
     label.htmlFor = "id";
@@ -51,18 +51,16 @@ function addTaskToList() {
 //Function that loops through elements of DOM (checboxes) and creating objects with two elements that is pushed into the array and stored in local storage.
 function saveList() {
   let listArray = [];
-  for (i = 0; i < $elements.length; i++) {
-    let isDone = false;
-    if ($elements[i].checked === true) {
-      isDone = true;
-    }
+  for (let i = 0; i < $elements.length; i++) {
+    const isDone = $elements[i].checked;
 
-    let taskName = $taskContent[i].textContent;
+    const taskName = $taskContent[i].textContent;
 
-    let task = {
-      taskName: `${taskName}`,
-      isDone: isDone,
+    const task = {
+      taskName,
+      isDone,
     };
+
     listArray.push(task);
     localStorage.setItem("array", JSON.stringify(listArray));
     console.log(listArray);
@@ -72,7 +70,7 @@ function saveList() {
 
 //Function that adds EventListeners to all checkboxes
 function addEventListenerToCheckbox() {
-  for (i = 0; i < $elements.length; i++) {
+  for (let i = 0; i < $elements.length; i++) {
     $elements[i].addEventListener("click", saveList);
   }
 }
@@ -86,10 +84,10 @@ function tasksCounter() {
 //Function gets array from local storage, finds "isDone" property and checks how many values "true" is in property "isDone". This function is added to saveList() with addEventListinerToCheckbox() so is enabled everytime checkbox is clicked.
 function doneTasksCounter() {
   let storedArray = JSON.parse(localStorage.getItem("array"));
-  let result = storedArray.map(({ isDone }) => isDone);
-  let doneTasks = result.filter((done) => done === true).length;
+  let doneTasksCount = storedArray.filter((item) => item.isDone).length;
 
-  document.querySelector(".tasksDone").innerHTML = "&nbsp" + `${doneTasks}`;
+  document.querySelector(".tasksDone").innerHTML =
+    "&nbsp" + `${doneTasksCount}`;
 }
 
 //Function RESET for button "Reset".-> Removes tasks from local storage, reloads page.
@@ -103,36 +101,32 @@ function resetFunction() {
 //Function that shows tasks saved in local storage after refresh page.
 
 window.onload = function () {
-  function loopArray() {
-    let taskFromArray = "";
-    for (i = 0; i < $storedArray.length; i++) {
-      taskFromArray = $storedArray[i];
+  let taskFromArray = "";
+  for (let i = 0; i < $storedArray.length; i++) {
+    taskFromArray = $storedArray[i];
 
-      let checkbox = document.createElement("input");
+    let checkbox = document.createElement("input");
 
-      checkbox.type = "checkbox";
-      checkbox.name = "name";
-      checkbox.value = "value";
-      checkbox.className = "id";
+    checkbox.type = "checkbox";
+    checkbox.name = "name";
+    checkbox.value = "value";
+    checkbox.className = "element";
 
-      if (taskFromArray.isDone === true) {
-        checkbox.checked = true;
-      } else {
-        checkbox.checked = false;
-      }
-
-      let label = document.createElement("label");
-
-      label.htmlFor = "id";
-      label.className = "label";
-      label.innerHTML = `${taskFromArray.taskName} <br/>`;
-
-      $myTaskList.appendChild(checkbox);
-      $myTaskList.appendChild(label);
+    if (taskFromArray.isDone) {
+      checkbox.checked = true;
+    } else {
+      checkbox.checked = false;
     }
-  }
 
-  loopArray();
+    let label = document.createElement("label");
+
+    label.htmlFor = "id";
+    label.className = "label";
+    label.innerHTML = `${taskFromArray.taskName} <br/>`;
+
+    $myTaskList.appendChild(checkbox, label);
+    $myTaskList.appendChild(label);
+  }
 
   addEventListenerToCheckbox();
 
